c9898badb8f5448389521cbbc7c0700693cf05e9
DeshanKTD <deshankalupahana@gmail.com>
2017-02-28 14:38:56 +0530

    auction table updating

--- routes/auctions.js
+++ routes/auctions.js
@@ -35,18 +35,19 @@ router.post('/new', function(req, res, next) {
             return res.status(500).json({success: false, data: err});
         }
 
-        data.vendors.forEach(function (vendor) {
-            client.query("INSERT INTO auction_vendors (auction_id, vendor_id) values ($1, $2)", [data.auc_id, vendor]);
-        });
-
-        data.vendors.forEach(function (item) {
-            client.query("INSERT INTO auction_items (auction_id, item_id) values ($1, $2)", [data.auc_id, item]);
-        });
 
         // SQL Query > Insert Data
         const query = client.query("INSERT INTO auction(auction_id, description, name, due_date, start_time, end_time, date_created, created_by) " +
             "values($1, $2, $3, $4, $5, $6, $7, $8)",
             [data.auc_id, data.descrip, data.name, data.due_date, data.s_time, data.e_time, '2017-02-25', data.cr_user]);
+        data.vendors.forEach(function (vendor) {
+            client.query("INSERT INTO auction_vendors (auction_id, vendor_id) values ($1, $2)", [data.auc_id, vendor]);
+        });
+
+        data.items.forEach(function (item) {
+            client.query("INSERT INTO auction_items (auction_id, item_id) values ($1, $2)", [data.auc_id, item]);
+        });
+
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -61,7 +62,36 @@ router.post('/new', function(req, res, next) {
     });
 });
 
-router.put('/edit', function(req, res, next) {
+router.post('/edit', function(req, res, next) {
+    const results = [];
+    // Get a Postgres client from the connection pool
+    const data = {
+        auid: req.body.auction_id,
+
+    };
+    console.log(req.body);
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+        // SQL Query > Select Data
+        const query = client.query("SELECT * FROM auction WHERE auction_id=($1)", [data.auid]);
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+});
+
+router.post('/edit/confirm', function(req, res, next) {
     const results = [];
     // Get a Postgres client from the connection pool
     const data = {
@@ -172,7 +202,7 @@ router.get('/initial', function(req, res, next) {
             return res.status(500).json({success: false, data: err});
         }
         // SQL Query > Insert Data
-        const query = client.query("SELECT auction_id,name,due_date FROM AUCTION WHERE due_date>CURRENT_DATE-1");
+        const query = client.query("SELECT auction_id,name,due_date FROM AUCTION");
         // Stream results back one row at a time
         query.on('row', function(row) {
             results.push(row);
@@ -185,6 +215,9 @@ router.get('/initial', function(req, res, next) {
         });
     });
 });
+
+
+
 
 
 module.exports = router;
